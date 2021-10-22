bbe53d8c65ff35b6426ea6844cd07a8471c5a01f
Paul Meems <bontepaarden@gmail.com>
2021-10-22 15:39:23 +0200

    Returning GeoJSON read from included file

--- api/parcels/index.js
+++ api/parcels/index.js
@@ -6,8 +6,10 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+        var result = require(__dirname + '//data//noord-groningen.json');
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: result
     };
 }
